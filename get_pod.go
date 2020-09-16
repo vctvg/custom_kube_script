899e473a027b78cfd22e63bc00610c9509721c27
vctvg <50947257+vctvg@users.noreply.github.com>
2020-09-16 17:27:12 +0800

    Update get_pod.go

--- get_pod.go
+++ get_pod.go
@@ -9,20 +9,19 @@ import (
 )
 
 func main() {
-	// uses the current context in kubeconfig
-	// path-to-kubeconfig -- for example, /root/.kube/config
+
 	config, err := clientcmd.BuildConfigFromFlags("", "config")
 
 	if err != nil{
 		panic(err.Error())
 	}
-	// creates the clientset
+
 	clientset, err := kubernetes.NewForConfig(config)
 
 	if err != nil{
 		panic(err.Error())
 	}
-	// access the API to list pods
+
 	pods, _ := clientset.CoreV1().Pods("kube-system").List(context.TODO(), metav1.ListOptions{})
 	fmt.Printf("There are %d pods in the cluster\n", len(pods.Items))
 }
